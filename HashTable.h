acc36fcc506077118ff9ede8862548ac1fadee77
AaronScheets <ascheets@carthage.edu>
2015-10-30 16:50:30 -0500

    implemented insert function

--- HashTable.h
+++ HashTable.h
@@ -14,15 +14,18 @@ class HashTable
     HashTable();
     ~HashTable();
 
- private:
+    void insert(const string& k, const T& v);
 
-    SafeArray < LinkedList <KeyValuePair <T> > > table;
+ private:
+    int hash(const string& k);
+    
+    SafeArray < LinkedList <KeyValuePair <T>* > > table;
 
 };
 
 //ctor
 template <class T>
-HashTable <T> :: HashTable()
+HashTable <T> :: HashTable() : table(101)
 {
 
 
@@ -33,5 +36,41 @@ template <class T>
 HashTable <T> :: ~HashTable()
 {
 
+
+}
+
+//insert
+template <class T>
+void HashTable <T> :: insert(const string& k, const T& v)
+{
+    //create a new KeyValuePair on the heap
+    KeyValuePair <T>* p_kvp = new KeyValuePair <T> (k,v);
+
+    //hash the key
+    int index = hash(k);
+
+    //insert into the table
+    table[index].insert(p_kvp);
+
+}
+
+//hash
+template <class T>
+int HashTable <T> :: hash(const string& k)
+{
+    unsigned int hashVal = 0;
+    int index;
+
+    for(int i = 0; i < k.size(); i++){
+		
+	//find the hash value before mod
+	hashVal = (hashVal + (int) k.at(i))*33;
+
+    }
+
+    //mod hash value by current size of table
+    index = hashVal % table.size();
+
+    return index;
 
 }
